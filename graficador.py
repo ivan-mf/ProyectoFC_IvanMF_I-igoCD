3549290fc3cf6b1886ccff6c5e8f40351b6c6ca7
Íñigo <inigo.casanova+git@gmail.com>
2025-12-08 09:36:34 -0600

    Actualizar graficador y resultados

--- graficador.py
+++ graficador.py
@@ -6,8 +6,10 @@ resultados_M_prom = np.load("Resultados/M_prom.npy")
 resultados_M_err = np.load("Resultados/M_err.npy")
 resultados_C_cal = np.load("Resultados/C_cal.npy")
 
+print(T)
+
 for M_prom, M_err, C_cal in zip(resultados_M_prom, resultados_M_err, resultados_C_cal):
-     plt.plot(T, M_prom)
-     plt.plot(T, C_cal)
+     plt.scatter(T, M_prom)
+     plt.scatter(T, C_cal)
 
      plt.show()
